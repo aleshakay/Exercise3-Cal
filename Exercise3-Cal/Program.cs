de39d3736963a208b2ea08301c0037cf2d3d142a
Alesha Reed <aleshak83@gmail.com>
2020-02-29 12:57:25 -0600

    Add project files.

---
+++ Exercise3-Cal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise3_Cal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
